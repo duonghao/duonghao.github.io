b42f78c16dbf601e632820e89f2afee160115c61
Hao Duong <hao.duong11@gmail.com>
2025-07-29 19:23:12 +0900

    refactor: temp site

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Hao's Portfolio",
+  description:
+    "Explore the portfolio of a web developer showcasing real-world projects, technical expertise, and hands-on experience in React, TypeScript, and cloud-based solutions. Discover case studies, code samples, and development insights.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
