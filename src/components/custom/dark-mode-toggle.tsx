c425622bd1ad77453158402c2b3e097a7d5709ff
Hao Duong <hao.duong11@gmail.com>
2025-08-02 20:54:20 +0900

    fix: dark mode toggle icon already displays based on theme using scale

--- src/components/custom/dark-mode-toggle.tsx
+++ src/components/custom/dark-mode-toggle.tsx
@@ -19,11 +19,8 @@ export function DarkModeToggle() {
 
   return (
     <Button variant="secondary" size="icon" onClick={onClickHandler}>
-      {theme === "light" ? (
-        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      ) : (
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-      )}
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
