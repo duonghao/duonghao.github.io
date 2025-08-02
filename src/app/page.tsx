5ca99b1a84ab8c8d50e321debb3b4af1e5e9a121
Hao Duong <hao.duong11@gmail.com>
2025-08-02 20:40:35 +0900

    feature: add dark theme control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,37 @@
+import { DarkModeToggle } from "@/components/custom/dark-mode-toggle";
 import { Linkedin, Github } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen min-w-screen md:grid md:grid-cols-2 md:gap-8 p-16 bg-amber-50/30">
-      <header className="">
-        <h3>Hello, World</h3>
-        <h1>Hao Duong</h1>
-        <p className="mt-2 mb-2">
-          I’m a software engineer who loves building clean, thoughtful solutions
-          to real-world problems. From crafting slick user interfaces to
-          streamlining developer workflows, I’ve worked across the stack with
-          React, TypeScript, and Java to make things work—and work well.
-        </p>
-        <ul className="flex gap-4">
-          <li>
-            <a href="https://www.linkedin.com/in/hao-duong-a87058196/">
-              <Linkedin />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/duonghao">
-              <Github />
-            </a>
-          </li>
-        </ul>
+    <>
+      <header className="flex justify-end">
+        <DarkModeToggle></DarkModeToggle>
       </header>
-      <div className="h-full">
-        <section className=" p-4 z-10 h-full">
+      <main className="flex flex-1 h-full items-center justify-center md:grid md:grid-cols-2 md:gap-8">
+        <header className="">
+          <h3>Hello, World</h3>
+          <h1>Hao Duong</h1>
+          <p className="mt-2 mb-2">
+            I’m a software engineer who loves building clean, thoughtful
+            solutions to real-world problems. From crafting slick user
+            interfaces to streamlining developer workflows, I’ve worked across
+            the stack with React, TypeScript, and Java to make things work—and
+            work well.
+          </p>
+          <ul className="flex gap-4">
+            <li>
+              <a href="https://www.linkedin.com/in/hao-duong-a87058196/">
+                <Linkedin />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/duonghao">
+                <Github />
+              </a>
+            </li>
+          </ul>
+        </header>
+        <section className="h-full flex flex-col p-4 z-10">
           <nav className="mb-4">
             <ul className="flex gap-4 md:gap-8">
               <li>Experience</li>
@@ -35,7 +40,7 @@ export default function Home() {
               <li>Photography</li>
             </ul>
           </nav>
-          <ul className="grid grid-cols-2 gap-2 h-full">
+          <ul className="grid grid-cols-2 gap-2 flex-1">
             <li>
               <div className="h-full w-full bg-gray-400"></div>
             </li>
@@ -50,7 +55,7 @@ export default function Home() {
             </li>
           </ul>
         </section>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
