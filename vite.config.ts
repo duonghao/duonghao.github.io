ecdc704217d00a4e430f822b44bf584f91a29d8f
Hao Duong <hao.duong11@gmail.com>
2026-08-26 17:03:31 +0900

    fix: base path isn't required

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,12 @@ import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-const REPO_NAME = 'duonghao.github.io'
-const BASE = process.env.NODE_ENV === 'production' ? `/${REPO_NAME}/` : '/'
-
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  base: BASE,
   plugins: [
     devtools(),
     tailwindcss(),
     tanstackStart({
-      router: {
-        basepath: BASE,
-      },
       spa: {
         enabled: true,
         prerender: {
